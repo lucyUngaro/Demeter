050c956270d8481aa02f67eccc128c05baee7330
lucyUngaro <lucyu12@gmail.com>
2020-02-02 08:11:15 -0500

    Hand shake

--- Assets/Scripts/PlayerInput/Chisel.cs
+++ Assets/Scripts/PlayerInput/Chisel.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class Chisel : MonoBehaviour
 {
     public float chiselSpeed;
+    public float handShake = 0.6f;
 
     private SculptablePart selectedPart; 
+
+    private float moveSpeed = 0f;
+    private float timer = 0f;
+    private float xLerp = 0f;
+    private float yLerp = 0f;
+
 
     void Update()
     {
@@ -16,6 +23,29 @@ public class Chisel : MonoBehaviour
 
         transform.Translate(x, 0, 0);
         transform.Translate(0, y, 0);
+
+        if (x == 0 && y == 0 && timer <= 0)
+        {
+            moveSpeed = Random.Range(0.3f, 1f);
+            timer = Random.Range(0.3f, 0.8f);
+
+            xLerp = transform.position.x + Random.Range(-handShake, handShake);
+            yLerp = transform.position.y + Random.Range(-handShake, handShake);
+        }
+
+        if (timer > 0)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(xLerp, yLerp, 0f), moveSpeed * Time.deltaTime);
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                timer = 0;
+            }
+        }
+        
+
+       
     }
 
     public void SelectPart(SculptablePart sp)
