2e9febaf87c825c50f324042553c1458f4715b7e
Easton <eastonself@gmail.com>
2020-02-02 04:54:53 -0500

    I'm the original visual

---
+++ Assets/lightController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightController : MonoBehaviour
+{
+
+    //lerps everything in its list to the color of the sun
+    public Color sunrise, sunset, currentColor;
+    public Gradient sunriseGrad, sunsetGrad, currentGradient;
+    public List<SpriteRenderer> lightSprites = new List<SpriteRenderer>();
+    public List<ParticleSystem> lightParts = new List<ParticleSystem>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < lightSprites.Count; i++){
+            lightSprites[i].color = Color.Lerp(lightSprites[i].color, currentColor, Time.deltaTime);
+        }
+
+        for (int i = 0; i < lightParts.Count; i++)
+        {
+            var main = lightParts[i].main;
+            Color partColor = Color.Lerp(main.startColor.colorMin, currentColor, Time.deltaTime);
+            main.startColor = new ParticleSystem.MinMaxGradient(partColor, partColor);
+        }
+
+        currentColor = Color.Lerp(sunrise, sunset, Mathf.PingPong(Time.time/15f, 1));
+    }
+}
---
+++ Assets/scrollUnroll.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollUnroll : MonoBehaviour
+{
+    public bool rolled;
+    public float dropHeight, speed, startingHeight;
+    public int prompt;
+    public List<SpriteRenderer> promptImages = new List<SpriteRenderer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rolled = true;
+        prompt = 0;
+        startingHeight = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos;
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rolled = !rolled;
+            prompt++;
+        }
+
+        if(rolled){
+            newPos = new Vector3(transform.position.x, startingHeight, transform.position.z);
+
+        }else{
+            newPos = new Vector3(transform.position.x, startingHeight - dropHeight, transform.position.z);
+            
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * speed);
+    }
+}
